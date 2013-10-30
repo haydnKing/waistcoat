ca144713e11c6c2bf03207b8c9d322a442b78f09
Haydn King <hjking734@gmail.com>
2013-10-30 18:18:14 +0000

    Test argument formatting

--- waistcoat/command.py
+++ waistcoat/command.py
@@ -1,32 +1,41 @@
 """Class to execute an external command after checking arguments etc"""
 
-from subprocess import Popen, PIPE, STDOUT, call
-import os
+import subprocess, os
 
 class Command:
 	"""A class which calls an external command and checks return values"""
 
 	#the command to call
 	cmd = ""
-	def __init__(self, command):
-		self.cmd = command
 
 	def call(self, switches=[], args={}, update_fn=None):
 		"""Call the function with the argument and check return codes"""
 
-		p = Popen([self.cmd, self.getArgs(switches, args)],
-							stdout=PIPE, stdin=PIPE, stderr=PIPE)
+		print "Call: {}".format([self.cmd,] + self.getArgs(switches, args))
+		p = subprocess.Popen([self.cmd,] + self.getArgs(switches, args),
+							stdout = subprocess.PIPE, 
+							stdin  = subprocess.PIPE,
+							stderr = subprocess.PIPE)
 
 		#wait for lines
 		if hasattr(update_fn, '__call__'):
 			while True:
-				nextline = process.stdout.readline()
+				nextline = p.stdout.readline()
 				if nextline == '' and p.poll() != None:
 					break
 				update_fn(nextline)
 
 		#collect the output
 		out = p.communicate()
+
+		#check returncode
+		if p.returncode < 0:
+			#error
+			raise subprocess.CalledProcessError(
+				"Program \"{}\" returned error code {}".format(cmd, p.returncode),
+				out[1])
+
+		return out
 
 	def getArgs(self, switches=[], args={}):
 		"""Return the argument list"""
@@ -35,16 +44,19 @@ class Command:
 				return "--{}".format(arg)
 			return "-{}".format(arg)
 
+		if isinstance(switches, basestring):
+			switches = [switches,]
+
 		ret = [format(s) for s in switches]
 
-		for k, v in kwargs.iteritems():
+		for k, v in args.iteritems():
 			ret += [format(k), v]
 
 		return ret
 				
 def isAvailable(command):
 	try:
-		call([command,])
+		subprocess.call([command,])
 	except OSError as e:
 		if e.errno == os.errno.ENOENT:
 			# handle file not found error.
