0ac902cafbaa873d5b44caf70bb72081b54f96f0
Haydn King <hjking734@gmail.com>
2013-11-01 12:25:43 +0000

    Check for tophat program

--- waistcoat/tophat.py
+++ waistcoat/tophat.py
@@ -4,4 +4,25 @@ import command
 
 class TopHat(command.Command):
 	"""Class to which interfaces with tophat"""
-	pass
+	def __init__(self):
+		self.cmd = "tophat"
+
+
+def test_programs():
+	commands = {'tophat': False, 
+		    'bowtie2': False, 
+		    'bowtie2-inspect': False, 
+		    'bowtie2-build': False, 
+		    'samtools': False,}
+	for cmd in commands.keys():
+		commands[cmd] = command.isAvailable(cmd)
+
+	errors = [cmd for cmd,available in commands.iteritems() if not available]
+
+	if errors:
+		raise ImportError("Required program(s) \'{}\' were not found.".format(
+			"\', \'".join(errors)))
+
+test_programs()
+		
+	
