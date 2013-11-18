2f0595a704ce815831ca1b833f8549ba6fafa1ca
Haydn King <hjking734@gmail.com>
2013-11-18 16:43:53 +0000

    Begin pipeline test

--- test/waistcoat_test.py
+++ test/waistcoat_test.py
@@ -1,1 +1,31 @@
+import unittest, os.path, tempfile, shutil, pysam
+from waistcoat import waistcoat
+
+DATA_DIR = os.path.join(os.path.split(__file__)[0], "data/")
+
+class PipelineTest(unittest.TestCase):
+	"""Test the pipeline"""
+	settings_file = os.path.join(DATA_DIR, 'pipeline_data/settings.json')
+	reads = os.path.join(DATA_DIR, 'pipeline_data/reads.fq')
+
+
+	@classmethod
+	def setUpClass(self):
+		self.tempdir = tempfile.mkdtemp(prefix='test')
+		waistcoat.run(self.settings_file, self.reads, self.tempdir)
+
+	@classmethod
+	def tearDownClass(self):
+		shutil.rmtree(self.tempdir)
+		self.tempdir = None
+
+	def test_output(self):
+		"""Test the pipeline outputs the correct files"""
+		print "Output: {}".format(', '.join(os.listdir(self.tempdir)))
+		self.assertTrue(os.path.exists(os.path.join(self.tempdir, 'sample_1.bam')), 
+			"Sample 1 output file missing")
+		self.assertTrue(os.path.exists(os.path.join(self.tempdir, 'sample_2.bam')), 
+			"Sample 2 output file missing")
+
+		raise AssertionError("Haven't written this yet")	
 
--- waistcoat/waistcoat.py
+++ waistcoat/waistcoat.py
@@ -2,23 +2,40 @@
 
 import argparse
 
+import settings, tophat, preprocess
+
 def main():
 	
 	#parse command line
 	my_args = get_arguments()
+	run(my_args.settings, myargs.reads.fq.gz, output)
 
+def run(settings_file, reads, outdir):
 	#Read and validate settings for waistcoat
-	print "Reading settings from \'{}\'".format(my_args.settings)
-	settings = read_settings(my_args.settings)
+	print "Reading settings from \'{}\'".format(settings_file)
+	s = settings.loadf(settings_file)
 
 	#split by barcode
+	files = preprocess.split_by_barcode(reads, s)
 
 	#clean files
+	files = preprocess.clean_distributions(files, s)
 
-	#filter unmapped
+	#discard those which map to discard
+	for (index, dcs) in s.discard:
+		new_files = []
+		for f in files:
+			new_files.append(tophat.discard_mapped(f, index, settings = dcs))
+		files = new_files
 
 	#map to genome
-
+	th = tophat.tophat_from_settings(s.map_settings)
+	for f in files:
+		th.output_dir = os.path.join(outdir, os.path.basename(f).split('.')[0])
+		os.mkdir(th.output_dir)
+		th.run(f, index_base = s.map)
+		os.remove(f)
+	
 	#index output SAM file
 
 
